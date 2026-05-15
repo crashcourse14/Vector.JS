6d9242b3a7f3d7f1e5ca15be23984710a45872a8
C3real <haydyncm@gmail.com>
2026-05-15 05:14:52 +0000

    [C3real] Addded terminal commands

--- ReleaseBuild.sh
+++ ReleaseBuild.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
-LATEST_BUILD_VER="1.0.0"
 echo "Set a commit message:"
 read COMMIT_MSG 
 
-
-
-echo "$LATEST_BUILD_VER" >> latestVersion.txt
-echo "Updated latestVersion.txt"
 
 git init
 git add .
---
+++ bin/vector.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+import { spawn } from "child_process";
+
+const args = process.argv.slice(2);
+const latest_build_version = "1.0.0"
+
+const command = args[0];
+const flag = args[1];
+
+if (command === "start" && flag === "--DOM") {
+
+    console.log("[Vector] Starting DOM server on port 8000...");
+
+    const server = spawn(
+        "python3",
+        ["-m", "http.server", "8000"],
+        {
+            stdio: "inherit"
+        }
+    );
+
+    server.on("close", (code) => {
+        console.log(`[Vector] Server exited with code ${code}`);
+    });
+
+}
+
+if (command  == "-version") {
+    console.log("[Vector] version 1.0.1")
+}
