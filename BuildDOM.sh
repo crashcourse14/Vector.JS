93a477bc3effd3169f103fa8ffcc2498d075a8b8
C3real <haydyncm@gmail.com>
2026-05-15 06:14:42 +0000

    [C3real]: created commit.sh file

--- BuildDOM.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "Setting up a http server (python) to test the DOM API..."
-
-python3 -m http.server 8000
---
+++ commit.sh
@@ -0,0 +1,8 @@
+author=$(git log -1 --pretty=%an)
+
+echo "Commit Message:"
+read COMMIT_MSG
+
+git init
+git add .
+git commit -m "[$author]: $COMMIT_MSG"
